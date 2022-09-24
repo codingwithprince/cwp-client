f24273d0e4d7c8938ce4e4155424c161d874b160
Prince Ahmed <codingwithprince@gmail.com>
2022-09-24 16:30:07 +0600

    added

--- components/Projects/Projects.js
+++ components/Projects/Projects.js
@@ -1,21 +1,50 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 
-
-
+const projectList = [
+  {
+    url: "/images/projects/jagger.png",
+  },
+  {
+    url: "/images/projects/wedding-glory.png",
+  },
+];
 
 const Projects = () => {
   return (
-    <div className="text-gray-200" id="projects">
-        <div className="title">
-            <h2 className="text-center text-2xl font-bold underline">Projects</h2>
-        </div>
-        <div className="images py-5 flex items-center justify-center">
-          {/* <Image src={'/images/projects/jagger.png'} height={350} width={800} alt="Projects Mockup" /> */}
-         
-        </div>
+    <div className="text-gray-200 overflow-hidden" id="projects">
+      <div className="title">
+        <h2 className="text-center text-2xl font-bold underline">Projects</h2>
+      </div>
+      <div className="images py-10 flex items-center justify-center">
+        <Carousel
+          autoPlay={true}
+          showIndicators={false}
+          showArrows={false}
+          showStatus={false}
+          infiniteLoop={true}
+        >
+          {projectList.map((project, i) => (
+            <div key={i} className="flex w-full">
+              <Image
+                src={project.url}
+                height={350}
+                width={800}
+                alt="project-list"
+              />
+              <div className="description">
+                <p className="title text-2xl font-semibold text-white text-center">
+                  Coming soon!!
+                </p>
+              </div>
+            </div>
+          ))}
+        </Carousel>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
